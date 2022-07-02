92a4e57595da01727f684886197ed4a914a52cbd
Nojukubi <nojukubi@yokize.com>
2022-07-02 21:19:03 +0300

    feat: storybook template

--- packages/framework/badge/WBadge.vue
+++ packages/framework/badge/WBadge.vue
@@ -15,9 +15,12 @@
 
   // Defines the props.
   const props = defineProps<{
-    inline?: boolean;
+    top?: boolean;
     left?: boolean;
+    right?: boolean;
     bottom?: boolean;
+    label?: string;
+    inline?: boolean;
     rounded?: boolean;
   }>();
 
@@ -29,26 +32,34 @@
     return props.inline ? true : !slots.target;
   });
 
-  // Create the class names based on the defined props.
+  // Create the class names based on the props.
   const classCss: object = $computed((): object => ({
     'w-badge--inline': isInline,
     'w-badge--rounded': props.rounded,
+    'w-badge--v-top': isInline && props.top,
+    'w-badge--v-bottom': isInline && props.bottom,
     'w-badge--left': !isInline && props.left,
-    'w-badge--bottom': !isInline && props.bottom
+    'w-badge--bottom': !isInline && props.bottom,
+    'w-badge--top': !isInline && (props.top || !props.bottom),
+    'w-badge--right': !isInline && (props.right || !props.left)
   }));
 </script>
 
 <style lang="sass" scoped>
-  @use '../node_modules/@stylize/sass-mixin/index' as *
+  @use '@stylize/sass-mixin' as *
 
   .w-badge
+    +absolute($z: 1)
     display: inline-flex
-    font-size: 12px
-    color: var(--badge-color, white)
     padding: 3px 6px
     border-radius: 4px
+    color: var(--badge-color, white)
+    font-size: var(--badge-font, 12px)
     background: var(--badge-bg, #1976d2)
-    +absolute($z: 1)
+
+    &-target
+      +relative
+      display: inline-block
 
     &--inline
       position: relative
@@ -56,18 +67,21 @@
     &--rounded
       border-radius: 14px
 
-    :not(&--inline)
-      top: 0
-      right: 0
+    &--top
+      top: -50%
+
+    &--right
+      right: -50%
 
     &--left
-      right: auto
-      left: 0
+      left: -50%
 
     &--bottom
-      top: auto
-      bottom: 0
+      bottom: -50%
 
-    &-target
-      +relative
+    &--v-top
+      vertical-align: top
+
+    &--v-bottom
+      vertical-align: bottom
 </style>
