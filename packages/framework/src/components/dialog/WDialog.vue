761a315439862862bc2e48a370699d46087796e5
Nojukubi <nojukubi@yokize.com>
2022-08-09 21:37:03 +0300

    feat: add base overlay and dialog

---
+++ packages/framework/src/components/dialog/WDialog.vue
@@ -0,0 +1,81 @@
+<template lang="pug">
+  w-overlay(
+    :absolute="props.absolute",
+    :transparent="!props.overlay",
+    @click="outsideClick")
+    component.w-dialog(
+      :is="props.tag",
+      :class="cssClasses")
+      slot(name="default")
+</template>
+
+<script lang="ts" setup>
+  import { WOverlay } from '../overlay';
+  // prettier-ignore
+  import { absoluteProp, boolProp, bottomProp, heightProp,
+    modelProp, leftProp, rightProp, tagProp, topProp,
+    widthProp, usePropsCssClasses } from '../../composables';
+
+  // Defines the props.
+  const props = defineProps({
+    ...tagProp(),
+    ...topProp(),
+    ...rightProp(),
+    ...bottomProp(),
+    ...leftProp(),
+    ...widthProp(),
+    ...heightProp(),
+    ...absoluteProp(),
+    ...modelProp(Boolean, false),
+    overlay: boolProp(true)
+  });
+
+  // Defines the emits.
+  const emits = defineEmits<{
+    (e: 'before-close'): void;
+    (e: 'update:modelValue', value: boolean): void;
+    (e: 'after-close'): void;
+  }>();
+
+  // prettier-ignore
+  // Composable to handle the CSS Classes.
+  const { absoluteCssClass, topCssClass, leftCssClass,
+    rightCssClass, bottomCssClass } = usePropsCssClasses();
+
+  // Create the CSS classes based on context.
+  const cssClasses: object = $computed(() => [
+    absoluteCssClass.value,
+    topCssClass.value,
+    rightCssClass.value,
+    bottomCssClass.value,
+    leftCssClass.value
+  ]);
+
+  // Handler to emit events on click.
+  function outsideClick(): void {
+    emits('before-close');
+    emits('update:modelValue', !props.modelValue);
+    emits('after-close');
+  }
+</script>
+
+<style lang="sass" scoped>
+  @use '@stylize/sass-mixin' as *
+  @use './WDialog' as *
+
+  .w-dialog
+    align-self: center
+    justify-self: center
+
+    &--top
+      justify-self: flex-start
+
+    &--right
+      align-self: flex-end
+
+    &--bottom
+      justify-self: flex-end
+
+    &--left
+      align-self: flex-start
+</style>
